ba26acd861fc80813a65f677ff7ccfb944e75cbd
ArturoGuerra <arturoguerra360@gmail.com>
2017-03-06 17:37:12 +0000

    Added things

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+sudo apt install zsh git curl
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/powerline/fonts/master/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+mv zshrc/myzshrc ~/.zshrc
+mv vimrc/myvimrc ~/.vimrc
+exit
