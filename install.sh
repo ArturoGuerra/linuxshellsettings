e419cf783dac7e4af6556b2e7d1c2dd27ed3f707
ArturoGuerra <arturoguerra360@gmail.com>
2017-03-06 18:31:48 +0000

    WIP

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/powerline/fonts/master/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/linuxshellsettings/master/zshrc/myzshrc)"
+sh -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/linuxshellsettings/master/zshrc/myvimrc)"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-sh -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/linuxshellsettings/master/zshrc/myzshrc)"
-sh -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/linuxshellsettings/master/zshrc/myvimrc)"
