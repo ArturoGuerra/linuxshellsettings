830855ba8510e5619d03a053684cf29cf96b652f
ArturoGuerra <arturoguerra360@gmail.com>
2017-03-06 18:15:23 +0000

    universal

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-
-sudo apt install zsh git curl
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/powerline/fonts/master/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
