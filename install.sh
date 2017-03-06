b4e81bd41bd3d298da27c989574d01cf7fb47fcd
ArturoGuerra <arturoguerra360@gmail.com>
2017-03-06 17:53:08 +0000

    W.I.P

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sudo apt install zsh git curl
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/powerline/fonts/master/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-exit
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
@@ -14,4 +13,3 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 git clone https://github.com/ArturoGuerra/linuxshellsettings/tree/master/zshrc/myzshrc ~/.zshrc
 git clone https://github.com/ArturoGuerra/linuxshellsettings/tree/master/zshrc/myvimrc ~/.vimrc
-exit
