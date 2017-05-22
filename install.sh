b43ebde91dba5c08a2c1c31b8ee6b57bf1f7be80
Arturo Guerra <arturoguerra360@gmail.com>
2017-05-22 16:44:50 -0500

    Fixed Shit

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ArturoGuerra/dotzsh/master/install.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ArturoGuerra/dotvim/master/install.sh)"
+git clone git@github.com:ArturoGuerra/dotvim.git ~/.vim
