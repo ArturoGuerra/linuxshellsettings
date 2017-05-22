76f39920c8f703b64389478eea58d10de8837588
ArturoGuerra <arturoguerra360@gmail.com>
2017-05-22 18:58:12 +0000

    Better system

--- manual/install.sh
+++ manual/install.sh
@@ -9,4 +9,14 @@ else
    sh -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/vimrc)"
 fi
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/bronson/vim-trailing-whitespace ~/.vim/bundle/vim-trailing-whitespace
+git clone https://github.com/matze/vim-move ~/.vim/bundle/vim-move
+git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive
+git clone https://github.com/Yggdroot/indentLine ~/.vim/bundle/indentLine
+git clone https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle/vim-markdown
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
