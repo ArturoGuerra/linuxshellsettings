2ee5b37d31440bfe33f36ca96ce2825cf746f880
Arturo Guerra <arturoguerra360@gmail.com>
2017-05-22 16:32:38 -0500

    Working

--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
-if [[ `uname` == 'Darwin' ]]; then
-   ruby -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-   ruby -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/.zshrc)"
-   ruby -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/.vimrc)"
-else
-   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-   sh -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/.zshrc)"
-   sh -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/.vimrc)"
-fi
-
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ArturoGuerra/dotzsh/master/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ArturoGuerra/dotvim/master/install.sh)"
--- manual/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [[ `uname` == 'Darwin' ]]; then
-   ruby -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/zshrc)"
-   ruby -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/vimrc)"
-else
-   sh -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/zshrc)"
-   sh -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/vimrc)"
-fi
-
