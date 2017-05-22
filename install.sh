1ba7d1fdb077404c71265d1937d06615cc22bedb
ArturoGuerra <arturoguerra360@gmail.com>
2017-05-22 17:37:53 +0000

    Fixed shit

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
-chsh -s /bin/zsh
 if [[ `uname` == 'Darwin' ]]; then
-   ruby -c "$(curl -fsSL https://raw.githubusercontent.com/powerline/fonts/master/install.sh)"
    ruby -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
    ruby -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/zshrc)"
    ruby -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/vimrc)"
 else
-   sh -c "$(curl -fsSL https://raw.githubusercontent.com/powerline/fonts/master/install.sh)"
    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
    sh -c "$(curl -o ~/.zshrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/zshrc)"
    sh -c "$(curl -o ~/.vimrc https://raw.githubusercontent.com/ArturoGuerra/shellsettings/master/vimrc)"
 fi
-
-
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
